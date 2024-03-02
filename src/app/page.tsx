69bfa5ef76e06c3a8dd0973898d89bc5da737261
Wesleyss071299 <wesleysilvadesousa@gmail.com>
2024-03-02 17:41:15 -0300

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,12 +27,12 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center">
-      <span className="text-5xl md:text-7xl text-primary font-extrabold text-center select-none">
-        FEATURED RAFFLES
+      <span className="uppercase text-5xl md:text-7xl text-primary font-extrabold text-center select-none">
+        Ending Soonest
       </span>
       <div className="flex flex-wrap items-center justify-center gap-10 mt-10">
         {raffles
-          .sort((a, b) => b.ticketsSold - a.ticketsSold)
+          .sort((a: any, b: any) => (new Date(a.endTime) as any) - (new Date(b.endTime) as any))
           .slice(0, 3)
           .map((nft, index) => (
             <Card
@@ -49,26 +49,29 @@ export default function Home() {
             />
           ))}
       </div>
-      <span className="text-5xl md:text-7xl text-primary font-extrabold text-center select-none mt-10">
-        RAFFLES
+      <span className="uppercase text-5xl md:text-7xl text-primary font-extrabold text-center select-none mt-10">
+        Hottest
       </span>
       {/* <div className="carousel carousel-center w-full max-w-7xl p-4 space-x-6 rounded-box"> */}
       <div className="flex flex-wrap w-full max-w-7xl p-4 gap-5 items-center justify-center rounded-box">
-        {raffles.map((nft, index) => (
-          <div key={index} className="carousel-item">
-            <Card
-              id={nft.id}
-              imgUrl={nft.image}
-              name={nft.name}
-              startTime={nft.startTime}
-              endTime={nft.endTime}
-              collectionName={nft.collectionName}
-              price={nft.price}
-              ticketsSold={nft.ticketsSold}
-              creator={nft.creator}
-            />
-          </div>
-        ))}
+        {raffles
+          .sort((a, b) => b.ticketsSold - a.ticketsSold)
+          .slice(0, 3)
+          .map((nft, index) => (
+            <div key={index} className="carousel-item">
+              <Card
+                id={nft.id}
+                imgUrl={nft.image}
+                name={nft.name}
+                startTime={nft.startTime}
+                endTime={nft.endTime}
+                collectionName={nft.collectionName}
+                price={nft.price}
+                ticketsSold={nft.ticketsSold}
+                creator={nft.creator}
+              />
+            </div>
+          ))}
       </div>
     </main>
   );
