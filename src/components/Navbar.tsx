b7518425776811be950a051b46336d99e30f351f
Wesleyss071299 <wesleysilvadesousa@gmail.com>
2024-03-02 20:19:52 -0300

    fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,25 +6,9 @@ export default function Navbar() {
   const { accounts } = useWallet();
 
   return (
-    <div className="navbar bg-transparent px-5 py-0">
+    <div className="navbar bg-transparent px-1 py-0">
       <div className="navbar-start">
         <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
-            </svg>
-          </div>
           <ul
             tabIndex={0}
             className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
